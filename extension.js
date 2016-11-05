08d73a5b5a1801d26d57061cb7072cace63acff6
Christian Müller <christian.mueller@centralway.com>
2016-11-05 11:35:11 +0100

    adds goTestsAll command

--- extension.js
+++ extension.js
@@ -1,30 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-let vscode = require('vscode');
+'use strict';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let vscode = require('vscode');
+let cp = require('child_process');
+
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gotests-vsc" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    console.log('gotests-vsc started');
+    
+    let disposable = vscode.commands.registerCommand('extension.goTestsAll', function () {
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let cmd = 'gotests';
+        let args = ['-all', '-w', fileName];
+        let result = cp.execFileSync(cmd, args);
+        vscode.window.showInformationMessage("Unit tests added successfully!");
     });
 
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
